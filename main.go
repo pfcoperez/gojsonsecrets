26b7f540b6dd486e851a7bcd45ed4796a5fd8fab
Pablo Fco. Perez Hidalgo <contact@pablofranciscoperez.info>
2023-04-13 23:41:09 +0200

    Generic implementation with configurable redaction setting

--- main.go
+++ main.go
@@ -5,33 +5,38 @@ import (
 	"fmt"
 )
 
+var JsonRedactSecrets bool = true
+
 type Secret[T any] struct {
-	HiddenValue T
+	HiddenValue   T
+	redactedValue T
 }
 
-type SecretString Secret[string]
-
-func AsSecretString(s string) SecretString {
-	return SecretString{
+func AsSecretString(s string) Secret[string] {
+	return Secret[string]{
 		HiddenValue: s,
 	}
 }
 
-func (s SecretString) MarshalJSON() ([]byte, error) {
-	return json.Marshal(s.HiddenValue)
+func (s Secret[T]) MarshalJSON() ([]byte, error) {
+	var valueToMarshal interface{} = s.HiddenValue
+	if JsonRedactSecrets {
+		valueToMarshal = s.redactedValue
+	}
+	return json.Marshal(valueToMarshal)
 }
 
 type SampleStruct struct {
 	Name    string
-	Age     int
-	Address SecretString
+	Age     Secret[int]
+	Address Secret[string]
 }
 
 func main() {
 
 	sample := SampleStruct{
 		Name:    "pablo",
-		Age:     36,
+		Age:     Secret[int]{HiddenValue: 36},
 		Address: AsSecretString("earth"),
 	}
 
